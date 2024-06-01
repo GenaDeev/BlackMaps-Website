18943ba07cdf9967a959fe9e8b697f2fd171f70c
GenaAaaj <genaroelgenio@hotmail.com>
2024-06-01 11:40:18 -0300

    mix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'custom': '0 4px 6px rgba(0, 0, 0, 0.1)',
+				'custom-lg': '0 10px 15px rgba(0, 0, 0, 0.25)',
+			}
+		}
 	},
 	plugins: [],
 }
