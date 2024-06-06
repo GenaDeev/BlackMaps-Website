dd89e694fbff81a7e172092b74df7e3af5028c25
GenaDev <genaroelgenio@hotmail.com>
2024-06-06 10:41:01 -0300

    Actualizar Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,12 @@
 import Logo from "./Logo.jsx"
+import Link from 'next/link'
+
 import ClientHeader from "./ClientHeader.jsx"
 export default function Header() {
     return (
         <header className="fixed w-full p-2 z-50 bg-[#ccc] dark:bg-[#242424] flex justify-between items-center">
             <div className="flex">
-                <a href="https://blackmaps.vercel.app"><Logo svgclass="dark:invert" width="48px" height="48px" className="logo text-white hover:scale-105 active:scale-95 transition active:bg-[#bbb] dark:active:bg-[#2d2d2d] dark:bg-[#3d3d3d] bg-[#aaa] rounded-xl p-2" /></a>
+                <Link href="/"><Logo svgclass="dark:invert" width="48px" height="48px" className="logo text-white hover:scale-105 active:scale-95 transition active:bg-[#bbb] dark:active:bg-[#2d2d2d] dark:bg-[#3d3d3d] bg-[#aaa] rounded-xl p-2" /></Link>
             </div>
             <ClientHeader/>
         </header>)
