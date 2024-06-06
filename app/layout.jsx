0bd4d35d3befda80efcd08f36714586a3ac27f05
GenaAaaj <genaroelgenio@hotmail.com>
2024-06-05 21:18:57 -0300

    Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }) {
   return (
@@ -64,6 +65,7 @@ export default function RootLayout({ children }) {
         <Header/>
         {children}
         <Analytics/>
+        <SpeedInsights/>
         <Footer/>
         </body>
     </html>
