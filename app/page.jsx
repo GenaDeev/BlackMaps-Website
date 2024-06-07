3011b11fd0b27532daadb159498f6e348dbc0944
GenaAaaj <genaroelgenio@hotmail.com>
2024-06-06 23:02:40 -0300

    seo v3

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
-import Link from 'next/link';
-import Logo from './components/Logo';
+import Link from "next/link";
+import Logo from "./components/Logo";
 
 export const metadata = {
     title: "Black Maps | Los mejores mapas, ahora online",
-    description: "Black Maps Web te proporciona acceso a todos tus mapas favoritos con la máxima calidad. Explora, descarga y disfruta de nuestros mapas únicos.",
-    keywords: ["Mapas", "Twitter", "BlackMaps", "Cartografía"],
+    description: "Black Maps Web te proporciona acceso a todos tus mapas favoritos con la máxima calidad. Explora, descarga y disfruta de nuestros mapas únicos, todos creados con un enfoque en la precisión y el detalle.",
+    keywords: ["Mapas", "Twitter", "BlackMaps", "Cartografía", "Exploración"],
     openGraph: {
         title: "Black Maps | Los mejores mapas, ahora online",
-        description: "Black Maps Web te proporciona acceso web a todos tus mapas favoritos permitiéndote descargarlos con la máxima calidad.",
-        images: 'https://blackmaps.vercel.app/og-home.png',
+        description: "Black Maps Web te proporciona acceso web a todos tus mapas favoritos permitiéndote descargarlos con la máxima calidad. Descubre un mundo de información y visualización sin precedentes.",
+        images: "https://blackmaps.vercel.app/og-home.png",
     },
     twitter: {
         card: "summary_large_image",
         title: "Black Maps | Los mejores mapas, ahora online",
-        description: "Black Maps Web te proporciona acceso web a todos tus mapas favoritos permitiéndote descargarlos con la máxima calidad.",
+        description: "Black Maps Web te proporciona acceso web a todos tus mapas favoritos permitiéndote descargarlos con la máxima calidad. Sumérgete en la experiencia visual que hemos preparado para ti.",
         creator: "@maps_black",
         creatorId: "1274191176267071490",
         images: ["https://blackmaps.vercel.app/og-home.png"],
@@ -31,9 +31,9 @@ export default function Home() {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-lg sm:p-0 p-12">
-                        <h1 className="mb-5 text-5xl font-bold">🌍<br/>¡Bienvenido/a a Black Maps!</h1>
+                        <h1 className="mb-5 text-5xl font-bold">🌍<br />¡Bienvenido/a a Black Maps!</h1>
                         <p className="mb-5 text-xl">
-                            Este es el sitio web oficial de <span className="font-bold">Black Maps</span>, una cuenta de X (antes Twitter) que se dedica a publicar mapas sobre todo tipo de temas. Si algo existe, le hacemos un mapa.
+                            Este es el sitio web oficial de <span className="font-bold">Black Maps</span>, una cuenta de X (antes Twitter) que se dedica a publicar mapas sobre todo tipo de temas. Si algo existe, le hacemos un mapa. Desde <span className="font-bold">geopolítica</span> hasta datos curiosos, aquí encontrarás un mapa para todo.
                         </p>
                         <div className="links flex gap-3 justify-center flex-wrap">
                             <Link title="Galería de Mapas" className="gap-2 flex items-center justify-center hover:scale-105 active:scale-95 transition dark:text-white text-[#1d1d1d] active:bg-[#bbb] dark:active:bg-[#2d2d2d] dark:bg-[#3d3d3d] bg-[#aaa] rounded-xl p-2 font-bold flex-shrink-1" href="/maps">Galería de Mapas</Link>
@@ -60,7 +60,7 @@ export default function Home() {
                     <div className="md:w-1/2">
                         <h2 className="text-[#2d2d2d] dark:text-[#eee] font-bold uppercase text-2xl sm:text-3xl md:text-4xl lg:text-6xl">Orígenes de Black Maps</h2>
                         <p className="text-[#3d3d3d] dark:text-gray-400">
-                            Black Maps subió su primer mapa el <span className="font-bold">20 de junio de 2020</span>, comenzando una emocionante historia que continúa hasta el día de hoy en <span className="font-bold">2024</span>. Su primer mapa publicado se llama <span className="font-bold">&quot;Life expectancy by country&quot;</span>, traducido como <span className="font-bold">&quot;Esperanza de vida por país&quot;</span>.
+                            <span className="font-bold">Black Maps</span> subió su primer mapa el <span className="font-bold">20 de junio de 2020</span>, marcando el inicio de una apasionante travesía en el mundo de la cartografía digital. A partir de ese momento, hemos crecido hasta convertirnos en un referente en la creación de <span className="font-bold">mapas únicos y detallados</span>, explorando temáticas diversas y ofreciendo una <span className="font-bold">perspectiva visual única</span>.
                         </p>
                     </div>
                 </div>
@@ -69,7 +69,7 @@ export default function Home() {
                     <div className="md:w-1/2">
                         <h2 className="text-[#2d2d2d] dark:text-[#eee] font-bold uppercase text-2xl sm:text-3xl md:text-4xl lg:text-6xl">¿Quién es Black Maps?</h2>
                         <p className="text-[#3d3d3d] dark:text-gray-400">
-                            Black Maps es el seudónimo de <span className="font-bold">Agustín Sánchez</span>, un joven <span className="font-bold">argentino</span> apasionado por la <span className="font-bold">cartografía</span>. Nació el <span className="font-bold">12 de diciembre</span> y es un gran fan del <span className="font-bold">Club Atlético River Plate</span>. Tiene <span className="font-bold">4 gatos</span>: dos marrones, uno tricolor y otro gris, aunque sus nombres son desconocidos.
+                            Black Maps es el seudónimo de <span className="font-bold">Agustín Sánchez</span>, un joven <span className="font-bold">argentino</span> con una profunda pasión por la <span className="font-bold">cartografía</span> y la visualización de datos. Nació el <span className="font-bold">12 de diciembre</span> y es un ávido seguidor del <span className="font-bold">Club Atlético River Plate</span>. Comparte su vida con <span className="font-bold">4 adorables gatos</span>: dos marrones, uno tricolor y otro gris, quienes inspiran su trabajo diario con su curiosa presencia.
                         </p>
                     </div>
                     <div className="flex mt-4 md:w-1/2 items-center justify-center md:justify-between flex-wrap gap-24">
@@ -81,6 +81,24 @@ export default function Home() {
                             width={400}
                             height={200}
                         />
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row w-full gap-8 bg-white dark:bg-[#3d3d3d] items-center justify-center rounded-xl p-12">
+                    <div className="flex mt-4 md:w-1/2 items-center justify-center md:justify-between flex-wrap gap-24">
+                        <Image
+                            loading="lazy"
+                            src="/map-example-1.webp"
+                            className="rounded-xl p-2 w-full hover:scale-105 transition duration-500 filter hover:drop-shadow-custom-lg"
+                            alt="Mapa de Black Maps llamado: Club de ESTADOS UNIDOS 🇺🇸 más buscado en Google por país (desde 2004)"
+                            width={400}
+                            height={200}
+                        />
+                    </div>
+                    <div className="md:w-1/2">
+                        <h2 className="text-[#2d2d2d] dark:text-[#eee] font-bold uppercase text-2xl sm:text-3xl md:text-4xl lg:text-6xl">¿Qué carajo sube?</h2>
+                        <p className="text-[#3d3d3d] dark:text-gray-400">
+                            Black Maps sube diversos tipos de <span className="font-bold">mapas</span>, desde los más <span className="font-bold">cómicos</span> hasta los más <span className="font-bold">serios</span>. Sus <span className="font-bold">mapas</span> abarcan múltiples <span className="font-bold">temas</span>, tanto <span className="font-bold">políticos</span>, <span className="font-bold">sociales</span> o simplemente <span className="font-bold">territoriales</span>. Uno de sus tipos de <span className="font-bold">mapa</span> más populares son aquellos donde utiliza el servicio de Google Trends para mostrar las <span className="font-bold">búsquedas más comunes</span> de los países.
+                        </p>
                     </div>
                 </div>
             </section>
