42fce61b88e96008a732d9ca65aaf683fcd35601
GenaAaaj <genaroelgenio@hotmail.com>
2024-06-06 23:11:06 -0300

    seo v4

--- app/maps/page.jsx
+++ app/maps/page.jsx
@@ -20,17 +20,17 @@ export const metadata = {
         canonical: 'https://blackmaps.vercel.app/maps',
     }
 };
+
 export default function MapsPage() {
 
     return (
         <main className="flex flex-col">
-            <div className="pt-20 px-12 sm:px-20 my-12 items-center flex flex-col">
+            <div className="pt-20 px-12 sm:px-20 my-12 items-center flex flex-col gap-4">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
-                    Black Maps - Galeria de Mapas
+                    Black Maps - Galería de Mapas
                 </h1>
                 <h2 className="text-md sm:text-lg md:text-xl lg:text-2xl">
-                    Todos los mapas publicados por Mapitas Negros desde la creacion
-                    de su cuenta.
+                    Explora una recopilación exhaustiva de todos los mapas que han sido compartidos por la vibrante comunidad de Mapitas Negros desde los albores de su cuenta hasta la fecha presente. Desde fascinantes diagramas urbanos hasta intrincadas representaciones cartográficas, esta galería ofrece una ventana única hacia la diversidad de narrativas geoespaciales que han surgido en el universo de los Mapitas Negros.
                 </h2>
                 <ClientSide />
             </div>
