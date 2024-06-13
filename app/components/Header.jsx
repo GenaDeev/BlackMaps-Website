42d9063816dd9ed7f11e9a1a1e488f79f2b20305
GenaDev <genaroelgenio@hotmail.com>
2024-06-13 08:20:53 -0300

    Actualizar Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import ClientHeader from "./ClientHeader.jsx"
 export default function Header() {
     return (
-        <header className="fixed w-full p-2 z-50 backdrop-blur-md border-b border-b-neutral-500 dark:border-neutral-800 flex justify-between items-center">
+        <header className="overflow-x-hidden sm:overflow-x-auto fixed w-full p-2 z-50 backdrop-blur-md border-b border-b-neutral-500 dark:border-neutral-800 flex justify-between items-center">
             <div className="flex">
                 <Link href="/"><Logo svgclass="dark:invert" width="48px" height="48px" className="logo text-white hover:scale-105 active:scale-95 transition active:bg-[#bbb] dark:active:bg-[#2d2d2d] dark:bg-[#4d4d4d] bg-[#aaa] rounded-xl p-2" /></Link>
             </div>
