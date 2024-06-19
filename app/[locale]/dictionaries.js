9a8968db72d3c69578eeb6961c4746bac3c7215e
GenaAaaj <genaroelgenio@hotmail.com>
2024-06-19 15:02:44 -0300

    sd

--- app/[locale]/dictionaries.js
+++
@@ -1,8 +0,0 @@
-import 'server-only'
- 
-const dictionaries = {
-  en: () => import('./dictionaries/es.json').then((module) => module.default),
-  nl: () => import('./dictionaries/en.json').then((module) => module.default),
-}
- 
-export const getDictionary = async (locale) => dictionaries[locale]()
