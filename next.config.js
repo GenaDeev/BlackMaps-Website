f138cde192a3c249606c923a4eb128095f30646a
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-17 15:04:13 -0300

    /

--- next.config.js
+++ next.config.js
@@ -39,6 +39,35 @@ module.exports = {
           }
         ],
       },
+      {
+        source: '/_next/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Powered-By',
+            value: ''
+          },
+          {
+            key: 'Server',
+            value: ''
+          }
+        ],
+      }
     ]
   },
 }
