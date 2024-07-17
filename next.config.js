b85c08b784109e1286fe2bb3bc23a9d73a5a6a66
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-17 15:05:24 -0300

    assd

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
         headers: [
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
+            value: 'max-age=31536000; includeSubDomains; preload'
           },
           {
             key: 'X-Content-Type-Options',
@@ -44,7 +44,7 @@ module.exports = {
         headers: [
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
+            value: 'max-age=31536000; includeSubDomains; preload'
           },
           {
             key: 'X-Content-Type-Options',
