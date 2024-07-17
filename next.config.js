881ea4fc42cf3c6da1f29a0c3e7ef0a2f4bc96d6
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-17 14:56:52 -0300

    Security Headers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
 module.exports = {
-    images: {
-        remotePatterns: [
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
           {
-            protocol: 'https',
-            hostname: 'pbs.twimg.com',
-            port: '',
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Powered-By',
+            value: ''
           },
         ],
       },
+    ]
+  },
 }
