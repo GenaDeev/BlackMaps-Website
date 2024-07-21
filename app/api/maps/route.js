02a57a4543c1061d8598cda622954b6ac33e61aa
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-21 00:51:46 -0300

    Fixed API Error

--- app/api/maps/route.js
+++ app/api/maps/route.js
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const res = await fetch('http://filehost.comidolar.com.ar:6060/api/blackmaps/mapgallery');
-    return NextResponse.json({
-        res
-      }, { status: 200 });
+    const res = await fetch('https://filehost.comidolar.com.ar:6060/api/blackmaps/mapgallery');
+    const body = await res.json();
+    return NextResponse.json(body, { status: 200 });
 }
