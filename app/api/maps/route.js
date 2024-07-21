eaa7d6ed254a260982b85fb6ef9fcf4d3db5dac7
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-21 00:07:48 -0300

    Try to fix fetch failed

--- app/api/maps/route.js
+++ app/api/maps/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const res = await fetch('https://filehost.comidolar.com.ar:6060/api/blackmaps/mapgallery');
+    const res = await fetch('http://filehost.comidolar.com.ar:6060/api/blackmaps/mapgallery');
     return NextResponse.json({
         res
       }, { status: 200 });
