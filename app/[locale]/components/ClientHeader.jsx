cbef23afbffb2b7afac321be46e90512665f35da
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-30 22:18:38 -0300

    Added loading spinner to color scheme change button 🎞️🔘

--- app/[locale]/components/ClientHeader.jsx
+++ app/[locale]/components/ClientHeader.jsx
@@ -96,8 +96,8 @@ export default function ClientHeader() {
     return (
         <nav>
             <ul className='flex gap-3'>
-                { colorScheme &&
-                    <li>
+                <li>
+                    {colorScheme ?
                         <button
                             title={lang === "es" ? schemeButtonTitle.es : schemeButtonTitle.en}
                             onClick={handleSchemeChange}
@@ -155,9 +155,12 @@ export default function ClientHeader() {
                                         :
                                         <></>
                             }
+                        </button> :
+                        <button className="w-12 h-12 flex items-center justify-center sm:hover:scale-105 active:scale-95 transition rounded-xl p-2 active:bg-[#2d2d2d] bg-[#4d4d4d]">
+                            <svg className="spinner_z9k8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"/></svg>
                         </button>
-                    </li>
-                }
+                    }
+                </li>
                 <li>
                     <div className="langselect gap-1 flex w-12 h-12 sm:w-auto items-center justify-center sm:hover:scale-105 active:scale-95 transition dark:text-white text-[#1d1d1d] active:bg-[#bbb] dark:active:bg-[#2d2d2d] dark:bg-[#4d4d4d] bg-[#aaa] rounded-xl p-2 font-bold">
                         <span className={`flag-icon flag-icon-${lang === "en" ? "en" : "es"} block`}></span>
