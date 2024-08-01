d492ee6740435d852e8adcc1cdaf1e52af85124a
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-31 21:22:09 -0300

    Removed unused import ❌

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,7 +1,6 @@
 import "../globals.css";
 import Footer from './components/Footer';
 import Header from './components/Header';
-import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import i18nConfig from '@/i18nConfig';
 
